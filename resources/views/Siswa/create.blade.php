e097ead858d75de47ab4df93309c073f8f58810e
Rafi Putra N <bartienayar04@gmail.com>
2026-06-13 23:50:01 +0700

    Siap EAS

--- resources/views/Siswa/create.blade.php
+++ resources/views/Siswa/create.blade.php
@@ -1,116 +1,57 @@
 @extends('templatebt5')
-@section('title', 'Data Siswa')
+@section('judul_halaman', 'Data Siswa')
 @section('konten')
+    <a href="/siswa" class="btn btn-secondary mb-4">Kembali</a>
 
-    <h2>Tambah Siswa</h2>
+    <div class="card">
+        <div class="card-header">
+            Form Tambah Data Siswa
+        </div>
 
-    @if ($errors->any())
-        <ul style="color: red;">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
+        <div class="card-body">
+            <form action="{{ route('siswa.store') }}" method="post">
+                {{ csrf_field() }}
+                <input type="hidden" name="_method" value="POST">
 
-    <form action="{{ route('siswa.store') }}" method="POST" onsubmit="return validasiForm()">
-        @csrf
+                <div class="row mb-3">
+                    <label for="NRP" class="col-sm-2 col-form-label">NRP</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="NRP" id="NRP" class="form-control" required
+                            value="{{ old('NRP') }}">
+                    </div>
+                </div>
 
-        <p>
-            <label>NRP</label><br>
-            <input type="text" name="NRP" id="NRP" maxlength="10" value="{{ old('NRP') }}">
-        </p>
+                <div class="row mb-3">
+                    <label for="Nama" class="col-sm-2 col-form-label">Nama</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="Nama" id="Nama" class="form-control" required
+                            value="{{ old('Nama') }}">
+                    </div>
+                </div>
 
-        <p>
-            <label>Nama</label><br>
-            <input type="text" name="Nama" id="Nama" maxlength="20" value="{{ old('Nama') }}">
-        </p>
+                <div class="row mb-3">
+                    <label for="Kelas" class="col-sm-2 col-form-label">Kelas</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="Kelas" id="Kelas" class="form-control" required
+                            value="{{ old('Kelas') }}">
+                    </div>
+                </div>
 
-        <p>
-            <label>Kelas</label><br>
-            <input type="text" name="Kelas" id="Kelas" maxlength="5" value="{{ old('Kelas') }}">
-        </p>
+                <div class="row mb-3">
+                    <label for="TanggalLahir" class="col-sm-2 col-form-label">Tanggal Lahir</label>
+                    <div class="col-sm-10">
+                        <input type="date" name="TanggalLahir" id="TanggalLahir" class="form-control" required
+                            value="{{ old('TanggalLahir') }}">
+                    </div>
+                </div>
 
-        <p>
-            <label>Tanggal Lahir</label><br>
-            <input type="date" name="TanggalLahir" id="TanggalLahir" value="{{ old('TanggalLahir') }}">
-        </p>
+                <div class="row">
+                    <div class="offset-sm-2 col-sm-10">
+                        <input type="submit" value="Simpan Data" class="btn btn-primary">
+                    </div>
+                </div>
 
-        <button type="submit">Simpan</button>
-        <a href="{{ route('siswa.index') }}">Kembali</a>
-    </form>
-
-    <script>
-        function validasiForm() {
-            let nrp = document.getElementById('NRP').value.trim();
-            let nama = document.getElementById('Nama').value.trim();
-            let kelas = document.getElementById('Kelas').value.trim();
-            let tanggal = document.getElementById('TanggalLahir').value;
-
-            if (nrp === '') {
-                Swal.fire({
-                    title: "Kesalahan Input Data!",
-                    text: "NRP wajib diisi",
-                    icon: "error"
-                });
-                return false;
-            }
-
-            if (nrp.length > 10) {
-                Swal.fire({
-                    title: "Kesalahan Input Data!",
-                    text: "NRP maksimal 10 karakter",
-                    icon: "error"
-                });
-                return false;
-            }
-
-            if (nama === '') {
-                Swal.fire({
-                    title: "Kesalahan Input Data!",
-                    text: "Nama wajib diisi",
-                    icon: "error"
-                });
-                return false;
-            }
-
-            if (nama.length > 20) {
-                Swal.fire({
-                    title: "Kesalahan Input Data!",
-                    text: "Nama maksimal 20 karakter",
-                    icon: "error"
-                });
-                return false;
-            }
-
-            if (kelas === '') {
-                Swal.fire({
-                    title: "Kesalahan Input Data!",
-                    text: "Kelas wajib diisi",
-                    icon: "error"
-                });
-                return false;
-            }
-
-            if (kelas.length > 5) {
-                Swal.fire({
-                    title: "Kesalahan Input Data!",
-                    text: "Kelas maksimal 5 karakter",
-                    icon: "error"
-                });
-                return false;
-            }
-
-            if (tanggal === '') {
-                Swal.fire({
-                    title: "Kesalahan Input Data!",
-                    text: "Tanggal lahir wajib diisi",
-                    icon: "error"
-                });
-                return false;
-
-            }
-
-            return true;
-        }
-    </script>
+            </form>
+        </div>
+    </div>
 @endsection
--- resources/views/Siswa/index.blade.php
+++ resources/views/Siswa/index.blade.php
@@ -1,5 +1,5 @@
 @extends('templatebt5')
-@section('title', 'Data Siswa')
+@section('judul_halaman', 'Data Siswa')
 @section('konten')
 
     <h2>Data Siswa</h2>
@@ -8,7 +8,7 @@
         <p style="color: green;">{{ session('success') }}</p>
     @endif
 
-    <a href="{{ route('siswa.create') }}">Tambah Siswa</a>
+    <a href="{{ route('siswa.create') }}" class="btn btn-primary mb-3">Tambah Siswa</a>
 
     <br><br>
 
